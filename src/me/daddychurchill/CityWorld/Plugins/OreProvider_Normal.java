e868a001e82c2d28de0bb5a4e16f033b234b0cf1
keenerb <none@specified.invalid>
2015-01-15 10:18:23 -0500

    Removing dirt/water generation from Destroyed world types

--- src/me/daddychurchill/CityWorld/Plugins/OreProvider_Normal.java
+++ src/me/daddychurchill/CityWorld/Plugins/OreProvider_Normal.java
@@ -28,6 +28,7 @@ public class OreProvider_Normal extends OreProvider {
 	 */
 	
 	private static final Material[] ore_types = new Material[] {Material.WATER,
+																Material.DIRT,
 																Material.LAVA,
 																Material.GRAVEL,
 																Material.COAL_ORE,
@@ -38,23 +39,33 @@ public class OreProvider_Normal extends OreProvider {
 																Material.DIAMOND_ORE,
 																Material.EMERALD_ORE}; 
 	
-	//                                                         WATER   LAVA   GRAV   COAL   IRON   GOLD  LAPIS  REDST   DIAM   EMER  
-	private static final int[] ore_iterations = new int[]    {     8,     6,    40,    30,    12,     4,     2,     4,     2,    10};
-	private static final int[] ore_amountToDo = new int[]    {     1,     1,    12,     8,     8,     3,     3,    10,     3,     1};
-	private static final int[] ore_maxY = new int[]          {   128,    32,   111,   128,    61,    29,    25,    16,    15,    32};
-	private static final int[] ore_minY = new int[]          {    32,     2,    40,    16,    10,     8,     8,     6,     2,     2};
-	private static final boolean[] ore_upper = new boolean[] {  true, false, false,  true,  true,  true,  true,  true, false, false};
-	private static final boolean[] ore_liquid = new boolean[] { true,  true, false, false, false, false, false, false, false, false};
+	//                                                         WATER   DIRT   LAVA   GRAV   COAL   IRON   GOLD  LAPIS  REDST   DIAM   EMER  
+	private static final int[] ore_iterations = new int[]    {     8,    20,     6,    40,    30,    12,     4,     2,     4,     2,    10};
+	private static final int[] ore_amountToDo = new int[]    {     1,    12,     1,    12,     8,     8,     3,     3,    10,     3,     1};
+	private static final int[] ore_maxY = new int[]          {   128,   128,    32,   111,   128,    61,    29,    25,    16,    15,    32};
+	private static final int[] ore_minY = new int[]          {    32,     1,     2,    40,    16,    10,     8,     8,     6,     2,     2};
+	private static final boolean[] ore_upper = new boolean[] {  true,  true, false, false,  true,  true,  true,  true,  true, false, false};
+	private static final boolean[] ore_liquid = new boolean[] { true, false,  true, false, false, false, false, false, false, false, false};
+
+	
 	
 	@Override
 	public void sprinkleOres(WorldGenerator generator, PlatLot lot, SupportChunk chunk, CachedYs blockYs, Odds odds, OreLocation location) {
-		
-		// do it!
-		for (int typeNdx = 0; typeNdx < ore_types.length; typeNdx++) {
-			sprinkleOre(generator, lot, chunk, blockYs,
-					odds, ore_types[typeNdx], ore_maxY[typeNdx], 
-					ore_minY[typeNdx], ore_iterations[typeNdx], 
-					ore_amountToDo[typeNdx], ore_upper[typeNdx], ore_liquid[typeNdx]);
+		if (generator.settings.includeDecayedNature) {
+			for (int typeNdx = 2; typeNdx < ore_types.length; typeNdx++) {
+				sprinkleOre(generator, lot, chunk, blockYs,
+						odds, ore_types[typeNdx], ore_maxY[typeNdx], 
+						ore_minY[typeNdx], ore_iterations[typeNdx], 
+						ore_amountToDo[typeNdx], ore_upper[typeNdx], ore_liquid[typeNdx]);
+			}
+		} else {
+			// do it!
+			for (int typeNdx = 0; typeNdx < ore_types.length; typeNdx++) {
+				sprinkleOre(generator, lot, chunk, blockYs,
+						odds, ore_types[typeNdx], ore_maxY[typeNdx], 
+						ore_minY[typeNdx], ore_iterations[typeNdx], 
+						ore_amountToDo[typeNdx], ore_upper[typeNdx], ore_liquid[typeNdx]);
+			}
 		}
 	}
 }
